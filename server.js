3dd303186e69983c531da6f20b7ae89400dc2e6d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-06-01 18:21:44 +0300

    Sent html page.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  console.log('Request is made.');
+  res.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('./views/index.html', (err, data) => {
+    if (err) {
+      console.log(err.message);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
 });
 
 server.listen(3000, () => {
