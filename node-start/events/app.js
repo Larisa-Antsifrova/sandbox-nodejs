08a93cb85ccb9268c24238239dbfc29e85898ab0
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-03 11:08:32 +0300

    Checked eventTarget methods.

--- node-start/events/app.js
+++ node-start/events/app.js
@@ -2,21 +2,21 @@ const EventEmitter = require("events");
 
 const myEmitter = new EventEmitter();
 
-const callback = data => {
-  console.log(`Received: ${data}`);
+const callbackEmitter = data => {
+  console.log(`EventEmitter. Received: ${data}`);
 };
 
 const once = () => {
   console.log("I have been called once!");
 };
 
-myEmitter.on("message", callback);
+myEmitter.on("message", callbackEmitter);
 myEmitter.emit("message", "Hello, testing events in NodeJS!");
-myEmitter.off("message", callback);
+myEmitter.off("message", callbackEmitter);
 
-myEmitter.addListener("event", callback);
+myEmitter.addListener("event", callbackEmitter);
 myEmitter.emit("event", "Checking other methods!");
-// myEmitter.removeListener("event", callback);
+// myEmitter.removeListener("event", callbackEmitter);
 // myEmitter.removeAllListeners("event");
 
 myEmitter.once("off", once);
@@ -39,3 +39,12 @@ myEmitter.on("error", error => {
 });
 
 myEmitter.emit("error", new Error("New error is here!"));
+
+const myTarget = new EventTarget();
+
+const callbackTarget = () => {
+  console.log("EventTarget.");
+};
+
+myTarget.addEventListener("connected", callbackTarget);
+myTarget.dispatchEvent(new Event("connected"));
