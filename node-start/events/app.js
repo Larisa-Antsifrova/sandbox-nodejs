08c14dd308e3034474a760106922adc64a01cef3
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-03 08:33:25 +0300

    Checked listeners methods.

--- node-start/events/app.js
+++ node-start/events/app.js
@@ -6,11 +6,36 @@ const callback = data => {
   console.log(`Received: ${data}`);
 };
 
+const once = () => {
+  console.log("I have been called once!");
+};
+
 myEmitter.on("message", callback);
 myEmitter.emit("message", "Hello, testing events in NodeJS!");
 myEmitter.off("message", callback);
 
 myEmitter.addListener("event", callback);
 myEmitter.emit("event", "Checking other methods!");
-myEmitter.removeListener("event", callback);
-myEmitter.removeAllListeners("event");
+// myEmitter.removeListener("event", callback);
+// myEmitter.removeAllListeners("event");
+
+myEmitter.once("off", once);
+myEmitter.emit("off");
+myEmitter.emit("off");
+
+console.log(myEmitter.getMaxListeners());
+myEmitter.setMaxListeners(5);
+console.log(myEmitter.getMaxListeners());
+console.log(myEmitter.listenerCount("event"));
+console.log(myEmitter.listenerCount("off"));
+
+console.log(myEmitter.listeners("event"));
+console.log(myEmitter.listeners("off"));
+
+console.log(myEmitter.eventNames());
+
+myEmitter.on("error", error => {
+  console.log("Error occured: " + error.message);
+});
+
+myEmitter.emit("error", new Error("New error is here!"));
