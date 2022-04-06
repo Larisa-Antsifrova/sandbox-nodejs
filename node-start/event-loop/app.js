772205622144354bf9b52ea3e679f7aecce674b3
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-06 20:13:33 +0300

    Explored timers methods.

--- node-start/event-loop/app.js
+++ node-start/event-loop/app.js
@@ -4,3 +4,18 @@ setTimeout(() => {
   console.log(performance.now() - start);
   console.log("A second passed!");
 }, 1000);
+
+function myFunc(arg) {
+  console.log("Argument =>", arg);
+}
+
+setTimeout(myFunc, 1500, "Here it goes!");
+
+const timerId = setTimeout(() => {
+  console.log("Finito!");
+}, 5000);
+
+setTimeout(() => {
+  console.log(`Clearing timer id: ${timerId}`);
+  clearTimeout(timerId);
+}, 1000);
