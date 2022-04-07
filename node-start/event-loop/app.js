64d7d66f7c2e5fcf5decfa067f58efc337a620b2
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-07 22:55:47 +0300

    Added methods for setInterval, setImmediate, ref, unref.

--- node-start/event-loop/app.js
+++ node-start/event-loop/app.js
@@ -19,3 +19,28 @@ setTimeout(() => {
   console.log(`Clearing timer id: ${timerId}`);
   clearTimeout(timerId);
 }, 1000);
+
+const intervalId = setInterval(() => {
+  console.log(performance.now());
+}, 1000);
+
+setTimeout(() => {
+  clearInterval(intervalId);
+  console.log("Done and over with!");
+}, 5000);
+
+console.log("Before");
+setImmediate(() => {
+  console.log("SetImmediate in action!");
+});
+console.log("After");
+
+const unrefTimerId = setTimeout(() => {
+  console.log("Unref timeout.");
+}, 500);
+
+unrefTimerId.unref();
+
+setImmediate(() => {
+  unrefTimerId.ref();
+});
