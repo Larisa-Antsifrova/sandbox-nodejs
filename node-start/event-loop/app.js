b645d56fb0f629c135ccae240651b9dd724427d1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-08 23:18:39 +0300

    Started example of order.

--- node-start/event-loop/app.js
+++ node-start/event-loop/app.js
@@ -1,46 +1,11 @@
-const start = performance.now();
+console.log("Start");
 
 setTimeout(() => {
-  console.log(performance.now() - start);
-  console.log("A second passed!");
-}, 1000);
-
-function myFunc(arg) {
-  console.log("Argument =>", arg);
-}
-
-setTimeout(myFunc, 1500, "Here it goes!");
-
-const timerId = setTimeout(() => {
-  console.log("Finito!");
-}, 5000);
-
-setTimeout(() => {
-  console.log(`Clearing timer id: ${timerId}`);
-  clearTimeout(timerId);
-}, 1000);
-
-const intervalId = setInterval(() => {
-  console.log(performance.now());
-}, 1000);
-
-setTimeout(() => {
-  clearInterval(intervalId);
-  console.log("Done and over with!");
-}, 5000);
-
-console.log("Before");
-setImmediate(() => {
-  console.log("SetImmediate in action!");
-});
-console.log("After");
-
-const unrefTimerId = setTimeout(() => {
-  console.log("Unref timeout.");
-}, 500);
-
-unrefTimerId.unref();
+  console.log("Timer 0", performance.now());
+}, 100);
 
 setImmediate(() => {
-  unrefTimerId.ref();
+  console.log("Immediate", performance.now());
 });
+
+console.log("Finish");
