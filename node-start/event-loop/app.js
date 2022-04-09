105fe765375bb66019f315ef8f25cef1f1922922
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-09 08:02:42 +0300

    Added more example of event loop order.

--- node-start/event-loop/app.js
+++ node-start/event-loop/app.js
@@ -1,11 +1,30 @@
+const fs = require("fs");
+
 console.log("Start");
 
 setTimeout(() => {
-  console.log("Timer 0", performance.now());
+  console.log("Timer 100", performance.now());
 }, 100);
 
 setImmediate(() => {
   console.log("Immediate", performance.now());
+});
+
+fs.readFile(__filename, () => {
+  console.log("File read!");
+});
+
+setTimeout(() => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log("Loop is over!");
+
+  Promise.resolve().then(() => {
+    console.log("Promise inside timeout");
+  });
+}, 0);
+
+Promise.resolve().then(() => {
+  console.log("Promise on the way!");
 });
 
 console.log("Finish");
