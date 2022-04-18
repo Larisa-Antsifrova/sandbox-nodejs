5ddc72c86fff1232070fee332e248c048b28e54e
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-18 22:39:30 +0300

    Added performance hooks example.

--- node-start/performance/app.js
+++ node-start/performance/app.js
@@ -1,17 +1,33 @@
+const perf_hooks = require("perf_hooks");
+
+const performanceObserver = new perf_hooks.PerformanceObserver(
+  (items, observer) => {
+    const entry = items.getEntriesByName("slow").pop();
+
+    console.log(`Entry name: ${entry.name}. Entry duration: ${entry.duration}`);
+
+    observer.disconnect();
+  },
+);
+
+performanceObserver.observe({
+  entryTypes: ["measure"],
+});
+
 function slow() {
   performance.mark("Start");
 
   const arr = [];
 
   for (let i = 0; i < 1000000; i++) {
-    arr.push(i + 1);
+    arr.push(i * i);
   }
 
   performance.mark("End");
 
-  performance.measure("Slow", "Start", "End");
+  performance.measure("slow", "Start", "End");
 
-  console.log(performance.getEntriesByName("Slow"));
+  console.log(performance.getEntriesByName("slow"));
 }
 
 slow();
