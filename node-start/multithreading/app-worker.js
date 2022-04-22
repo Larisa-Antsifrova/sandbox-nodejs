07504e0339b28daf413c9ba64278bed0040ec7d3
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-22 23:52:23 +0300

    Added new example.

---
+++ node-start/multithreading/app-worker.js
@@ -0,0 +1,43 @@
+const { Worker } = require("worker_threads");
+
+const compute = array => {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./worker.js", {
+      workerData: {
+        array,
+      },
+    });
+
+    worker.on("message", message => resolve(message));
+
+    worker.on("error", error => reject(error));
+
+    worker.on("exit", () => console.log("Worker is done!"));
+  });
+};
+
+const main = async () => {
+  try {
+    performance.mark("start");
+
+    const result = await Promise.all([
+      compute([20, 25, 30, 35, 40, 45, 50]),
+      compute([20, 25, 30, 35, 40, 45, 50]),
+      compute([20, 25, 30, 35, 40, 45, 50]),
+      compute([20, 25, 30, 35, 40, 45, 50]),
+    ]);
+
+    console.log("Result:", result);
+
+    performance.mark("end");
+    performance.measure("main", "start", "end");
+  } catch (error) {
+    console.error("Error: ", error.message);
+  }
+};
+
+setTimeout(() => {
+  console.log("Timeout in Main");
+}, 2000);
+
+main();
