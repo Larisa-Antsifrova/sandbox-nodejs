1011d77bef5a1bef11ff42776449a6816b6d3722
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-04-23 18:39:29 +0300

    Separated worker example from workerless one.

--- node-start/multithreading/app.js
+++ node-start/multithreading/app.js
@@ -1,36 +1,31 @@
-const { Worker } = require("worker_threads");
+const factorial = require("./factorial");
 
 const compute = array => {
-  return new Promise((resolve, reject) => {
-    const worker = new Worker("./worker.js", {
-      workerData: {
-        array,
-      },
-    });
+  const arr = [];
 
-    worker.on("message", message => resolve(message));
+  for (let i = 0; i < 1000000; i++) {
+    arr.push(i);
+  }
 
-    worker.on("error", error => reject(error));
-
-    worker.on("exit", () => console.log("Worker is done!"));
-  });
+  return array.map(el => factorial(el));
 };
 
-const main = async () => {
+const main = () => {
   try {
     performance.mark("start");
 
-    const result = await Promise.all([
+    const result = [
       compute([20, 25, 30, 35, 40, 45, 50]),
       compute([20, 25, 30, 35, 40, 45, 50]),
       compute([20, 25, 30, 35, 40, 45, 50]),
       compute([20, 25, 30, 35, 40, 45, 50]),
-    ]);
+    ];
 
     console.log("Result:", result);
 
     performance.mark("end");
     performance.measure("main", "start", "end");
+    console.log("Performance: ", performance.getEntriesByName("main").pop());
   } catch (error) {
     console.error("Error: ", error.message);
   }
