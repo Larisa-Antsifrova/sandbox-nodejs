ae6fcddfb950bec516ce675a5e4bb6fb08207251
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-05-09 21:23:26 +0300

    Checked out path methods.

--- node-start/weather-cli/weather.js
+++ node-start/weather-cli/weather.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import { printError, printSuccess, printHelp } from "./services/logService.js";
-
+import { saveKeyValue } from "./services/storageService.js";
 const initCLI = () => {
   const args = getArgs(process.argv);
   if (args.h) {
@@ -13,6 +13,7 @@ const initCLI = () => {
   }
 
   if (args.t) {
+    saveKeyValue("token", args.t);
     // Save the token
   }
 
