6eea045c93e68cd9c73d30ec9dccc47fc63bc0d7
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-05-13 22:43:55 +0300

    Added implementation with axios.

--- node-start/weather-cli/services/apiService.js
+++ node-start/weather-cli/services/apiService.js
@@ -1,4 +1,4 @@
-import https from "https";
+import axios from "axios";
 import { getKeyValue, TOKEN_DICTIONARY } from "./storageService.js";
 
 const getWeather = async city => {
@@ -8,22 +8,50 @@ const getWeather = async city => {
     throw new Error("No token found.");
   }
 
-  const url = new URL("https://api.openweathermap.org/data/2.5/weather");
-  url.searchParams.append("q", city);
-  url.searchParams.append("appid", token);
-  url.searchParams.append("units", "metric");
+  const { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/weather",
+    {
+      params: {
+        q: city,
+        appid: token,
+        units: "metric",
+      },
+    },
+  );
 
-  https.get(url, response => {
-    let result = "";
-
-    response.on("data", chunk => {
-      result += chunk;
-    });
-
-    response.on("end", () => {
-      console.log(result);
-    });
-  });
+  return data;
 };
 
 export { getWeather };
+
+//axios free example with https package
+
+// import https from "https";
+// import { getKeyValue, TOKEN_DICTIONARY } from "./storageService.js";
+
+// const getWeather = async city => {
+//   const token = await getKeyValue(TOKEN_DICTIONARY.token);
+
+//   if (!token) {
+//     throw new Error("No token found.");
+//   }
+
+//   const url = new URL("https://api.openweathermap.org/data/2.5/weather");
+//   url.searchParams.append("q", city);
+//   url.searchParams.append("appid", token);
+//   url.searchParams.append("units", "metric");
+
+//   https.get(url, response => {
+//     let result = "";
+
+//     response.on("data", chunk => {
+//       result += chunk;
+//     });
+
+//     response.on("end", () => {
+//       console.log(result);
+//     });
+//   });
+// };
+
+// export { getWeather };
