5743386fd5f71fb584c93ebfaf320a31a7037307
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-05-14 21:53:51 +0300

    Added env variable.

--- node-start/weather-cli/services/apiService.js
+++ node-start/weather-cli/services/apiService.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import { getKeyValue, TOKEN_DICTIONARY } from "./storageService.js";
 
 const getWeather = async city => {
-  const token = await getKeyValue(TOKEN_DICTIONARY.token);
+  const token =
+    process.env.TOKEN ?? (await getKeyValue(TOKEN_DICTIONARY.token));
 
   if (!token) {
     throw new Error("No token found.");
