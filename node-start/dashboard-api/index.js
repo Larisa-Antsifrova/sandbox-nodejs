95706bb007cc2f1c8277eadfa130dec2bd79f572
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-05-19 22:50:34 +0300

    Formed test response.

--- node-start/dashboard-api/index.js
+++ node-start/dashboard-api/index.js
@@ -4,9 +4,11 @@ const host = "127.0.0.1";
 const port = 8888;
 
 const server = http.createServer((request, response) => {
-  //
+  response.statusCode = 200;
+  response.setHeader("Content-Type", "text/plain");
+  response.end("Hi!");
 });
 
 server.listen(port, host, () => {
-  console.log(`Server is on port: ${port}`);
+  console.log(`Server is on ${host}: ${port}`);
 });
