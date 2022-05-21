1aabef4aaabf9a2671e498f44254d5e9d23e0b34
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2022-05-21 10:19:39 +0300

    Added switch case example for routes.

--- node-start/dashboard-api/index.js
+++ node-start/dashboard-api/index.js
@@ -4,9 +4,22 @@ const host = "127.0.0.1";
 const port = 8888;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader("Content-Type", "text/plain");
-  response.end("Hi!");
+  switch (request.method) {
+    case "GET":
+      switch (request.url) {
+        case "/hello":
+          response.statusCode = 200;
+          response.setHeader("Content-Type", "text/plain");
+          response.end("Hi!");
+          break;
+
+        default:
+          response.statusCode = 404;
+          response.setHeader("Content-Type", "text/plain");
+          response.end("NOthing Found!");
+      }
+      break;
+  }
 });
 
 server.listen(port, host, () => {
